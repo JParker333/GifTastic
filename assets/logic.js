ec03105734ade4a34f1103353dbc51b7fc86530b
JPark <J.parker333@icloud.com>
2018-09-08 20:22:33 -0600

    mostly done, need to fix bug still

--- assets/logic.js
+++ assets/logic.js
@@ -14,7 +14,7 @@ function populateButtons(searchArray, classToAdd, areaToAddTo) {
 	// empty out buttons area every time we add a new button to prevent copies of buttons
 	$(areaToAddTo).empty();
 
-	// lops through the list of cartoons
+	// loops through the list of cartoons
 	for (var i = 0; i < searchArray.length; i++) {
 
 		// modifiying button element
@@ -32,16 +32,22 @@ function populateButtons(searchArray, classToAdd, areaToAddTo) {
 
 // when the searchbutton is clicked
 $(document).on("click", ".searchButton", function () {
-	var type = $(this).data("type");
+	$("#searches").empty();
 
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=VDI1IDJjdm7DGBpfMAc58F85a37aDulQ&limit=10";
+	var type = $(this).data("type");
+	console.log(type);
+
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=VDI1IDJjdm7DGBpfMAc58F85a37aDulQ&limit=10";
+
 
 	// API call
 	$.ajax({
 		url: queryURL,
 		method: "GET"
-		// return reponse within a function
-	}).done(function (response) {
+		// 	// return reponse within a function
+	}).then(function (response) {
+		console.log(response);
+
 		// loops through data array
 		for (var i = 0; i < response.data.length; i++) {
 
@@ -56,18 +62,56 @@ $(document).on("click", ".searchButton", function () {
 
 			var animated = response.data[i].images.fixed_height.url;
 
-			var still = reponse.data[i].images.fixed_height_still.url;
+			var still = response.data[i].images.fixed_height_still.url;
 
 			// image tag
 			var image = $("<img>");
+
+			// modifiying the image
 			image.attr("src", still);
 			image.attr("data-still", still);
 			image.attr("data-animated", animated);
 			image.attr("data-state", "still");
 			image.addClass("searchImage");
+			// adding the above to a search div
+			// adding in the paragraph that tells us the rating
 			searchDiv.append(p);
+			// adding in the image for Gif
+			searchDiv.append(image);
+			// adding to within HTML
+			$("#searches").append(searchDiv);
 
 		}
 	})
+})
+
+// animating the gifs
+$(document).on("click", ".searchImage", function(){
+
+	var state = $(this).attr("data-state");
+	if (state==="still"){
+		$(this).attr("src", $(this).data("animated"))
+		$(this).attr("data-state", "animated");
+	} else {
+		$(this).attr("src", $(this).data("still"))
+		$(this).attr("data-state", "still");
+	}
+})
+
+// adding new buttons
+$("#addSearch").on("click", function () {
+
+	// grabs whatever is stored within text box and place in new search var
+	// eq looks for an input
+	var newSearch = $("input").eq(0).val();
+
+	// add to search array 
+	searchArray.push(newSearch);
+
+	populateButtons(searchArray, "searchButton", "#buttonsArea");
+
+	// return false because we have a submit type button
+	// so that we can prevent from re loading the page
+	return false;
 
 })
